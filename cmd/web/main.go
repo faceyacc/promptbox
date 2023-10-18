df79a295a687a971b1f3198444525687a2f26f3f
Ty <justfacey@gmail.com>
2023-10-18 19:24:49 -0400

    added a structured logger verus using Go's standard logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// Added for structured logging
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -15,7 +24,10 @@ func main() {
 	mux.HandleFunc("/prompt/view", promptView)
 	mux.HandleFunc("/prompt/create", promptCreate)
 
-	log.Print("Starting seerver on: http://localhost:4000/")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	logger.Info("starting server", slog.String("addr", *addr))
+
+	err := http.ListenAndServe(*addr, mux)
+
+	logger.Error(err.Error())
+	os.Exit(1)
 }
