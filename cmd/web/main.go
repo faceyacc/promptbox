ee71d65e840aca927f19e36193d349c1df842a91
Ty <justfacey@gmail.com>
2023-10-24 11:47:04 -0400

    encapsulated routing functionality to seperate file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -24,18 +24,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/prompt/view", app.promptView)
-	mux.HandleFunc("/prompt/create", app.promptCreate)
-
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 
 	logger.Error(err.Error())
 	os.Exit(1)
---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+// Return a servemux containg application routes
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/prompt/view", app.promptView)
+	mux.HandleFunc("/prompt/create", app.promptCreate)
+
+	return mux
+}
