59d84b62a7060bc1240ec474a6540b8280ba89bd
Ty <justfacey@gmail.com>
2023-12-01 12:52:29 -0500

    Added middleware to only allow logged in users to create prompts.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,15 +22,19 @@ func (app *application) routes() http.Handler {
 
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/prompt/view/:id", dynamic.ThenFunc(app.promptView))
-	router.Handler(http.MethodGet, "/prompt/create", dynamic.ThenFunc(app.promptCreate))
-	router.Handler(http.MethodPost, "/prompt/create", dynamic.ThenFunc(app.promptCreatePost))
 
 	// Routes to handle user authentication.
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
 	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
-	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
+	authenticated := dynamic.Append(app.requireAuthentication)
+
+	// authenticated-only app routes.
+	router.Handler(http.MethodGet, "/prompt/create", authenticated.ThenFunc(app.promptCreate))
+	router.Handler(http.MethodPost, "/prompt/create", authenticated.ThenFunc(app.promptCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", authenticated.ThenFunc(app.userLogoutPost))
 
 	requestMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
